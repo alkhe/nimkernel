24860b4dca1da4848dfc013b29a1fcdf99baf8fc
edge <sort@programmer.net>
2015-02-20 08:26:51 +0900

    update nakefile

--- nakefile.nim
+++ nakefile.nim
@@ -2,8 +2,8 @@ import nake
 import os
 
 const
-  CC = "i586-elf-gcc"
-  asmC = "i586-elf-as"
+  CC = "i686-elf-gcc"
+  asmC = "i686-elf-as"
 
 task "clean", "Removes build files.":
   removeFile("boot.o")
@@ -14,15 +14,15 @@ task "clean", "Removes build files.":
 task "build", "Builds the operating system.":
   echo "Compiling..."
   direShell "nim c -d:release --gcc.exe:$1 main.nim" % CC
-  
+
   direShell asmC, "boot.s -o boot.o"
-  
+
   echo "Linking..."
-  
-  direShell CC, "-T linker.ld -o main.bin -ffreestanding -O2 -nostdlib boot.o nimcache/main.o nimcache/stdlib_system.o nimcache/stdlib_unsigned.o nimcache/ioutils.o"
-  
+
+  direShell CC, "-T linker.ld -o main.bin -ffreestanding -O2 -nostdlib boot.o nimcache/main.o nimcache/system.o nimcache/unsigned.o nimcache/ioutils.o"
+
   echo "Done."
-  
+
 task "run", "Runs the operating system using QEMU.":
   if not existsFile("main.bin"): runTask("build")
   direShell "qemu-system-i386 -kernel main.bin"

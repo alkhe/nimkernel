28b4e4fde8d95ab417b49ac87a8de76fe40b94e6
edge <sort@programmer.net>
2015-02-20 08:51:05 +0900

    fix nakefile, change tab size to 4 spaces

--- nakefile.nim
+++ nakefile.nim
@@ -1,28 +1,25 @@
 import nake
 import os
 
+const arch = "i686-elf"
+
 const
-  CC = "i686-elf-gcc"
-  asmC = "i686-elf-as"
+    CC = arch & "-gcc"
+    AS = arch & "-as"
 
-task "clean", "Removes build files.":
-  removeFile("boot.o")
-  removeFile("main.bin")
-  removeDir("nimcache")
-  echo "Done."
+task "clean", "remove build files":
+    removeFile("boot.o")
+    removeFile("main.bin")
+    removeDir("nimcache")
 
-task "build", "Builds the operating system.":
-  echo "Compiling..."
-  direShell "nim c -d:release --gcc.exe:$1 main.nim" % CC
+task "build", "build the operating system":
+    echo "Compiling..."
+    direShell "nim c -d:release --gcc.exe:$1 main.nim" % CC
+    echo "Assembling..."
+    direShell AS, "boot.s -o boot.o"
+    echo "Linking..."
+    direShell CC, "-T linker.ld -o main.bin -ffreestanding -O2 -nostdlib boot.o nimcache/main.o nimcache/system.o nimcache/unsigned.o nimcache/ioutils.o"
 
-  direShell asmC, "boot.s -o boot.o"
-
-  echo "Linking..."
-
-  direShell CC, "-T linker.ld -o main.bin -ffreestanding -O2 -nostdlib boot.o nimcache/main.o nimcache/system.o nimcache/unsigned.o nimcache/ioutils.o"
-
-  echo "Done."
-
-task "run", "Runs the operating system using QEMU.":
-  if not existsFile("main.bin"): runTask("build")
-  direShell "qemu-system-i386 -kernel main.bin"
+task "run", "run the operating system using QEMU":
+    if not existsFile("main.bin"): runTask("build")
+    direShell "qemu-system-i386 -kernel main.bin"
--- panicoverride.nim
+++ panicoverride.nim
@@ -2,12 +2,12 @@ import ioutils
 {.push stack_trace: off, profiler:off.}
 
 proc rawoutput(s: string) =
-  var vram = cast[PVIDMem](0xB8000)
-  writeString(vram, "Error: ", makeColor(White, Red), (0, 24))
-  writeString(vram, s, makeColor(White, Red), (7, 24))
+    var vram = cast[PVIDMem](0xB8000)
+    writeString(vram, "Error: ", makeColor(White, Red), (0, 24))
+    writeString(vram, s, makeColor(White, Red), (7, 24))
 
 proc panic(s: string) =
-  rawoutput(s)
+    rawoutput(s)
 
 # Alternatively we also could implement these 2 here:
 #
